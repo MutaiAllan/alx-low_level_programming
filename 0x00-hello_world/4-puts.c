ebca359e57a68bd5a7490a61b4b1c9ca599082d8
MutaiAllan <allankiprop175@gmail.com>
2022-10-17 04:57:24 -0700

    low level

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+1 #include <stdio.h>
+2
+3 /**
+4  * main - prints "Programming is like building a multilingual puzzle
+5  * Return: 0
+6  */
+7 int main(void)
+8	{
+9	puts("\"Programming is like building a multilingual puzzle");
+10        return (0);
+11      }
