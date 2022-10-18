1644c0b3e1d31c480e8659fff1dd807df6b4b2b9
MutaiAllan <allankiprop175@gmail.com>
2022-10-18 01:57:02 -0700

    low level

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * return: 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
