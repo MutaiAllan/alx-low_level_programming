6cdb30395efbb7a1cab10ba40f3f7a1fb7ddfa7f
MutaiAllan <allankiprop175@gmail.com>
2022-10-20 09:08:54 -0700

    low level

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints single base 10 numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
