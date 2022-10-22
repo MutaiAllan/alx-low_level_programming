ba3477c6821206048f9375a3318d6bb7713c2ced
MutaiAllan <allankiprop175@gmail.com>
2022-10-22 02:09:37 -0700

    low level

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - prints and that piece of art is useful - Dora Korpar, 2015-10-19
+ * main - prints and that piece of art is useful" - Dora Korpar, 2015-10-19
  *
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
