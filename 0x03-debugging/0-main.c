daf19f163a008fb45fd3b1c6d55859a1dd37fed1
MutaiAllan <allankiprop175@gmail.com>
2022-10-24 14:38:55 -0700

    low level

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -9,8 +12,14 @@ int main(void)
 {
 	int i;
 
-	i = 98;
-	positive_or_negative(i);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 
 	return (0);
 }
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-#include "2-main.c"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
