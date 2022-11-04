b66771f8fabac5ec547b880c4e80a8381b197b79
MutaiAllan <allankiprop175@gmail.com>
2022-11-04 00:20:53 -0700

    product of two numbers

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
@@ -8,11 +7,6 @@
  */
 int main(void)
 {
-    char c;
-
-    c = '0';
-    printf("%c: %d\n", c, _isdigit(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isdigit(c));
+    print_numbers();
     return (0);
 }
