b9881f5b3898a65ba3d1c0c9286e457fd66bc35c
MutaiAllan <allankiprop175@gmail.com>
2022-11-03 23:59:37 -0700

    product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - gives the product of two numbers
+ * @a: inputs one number
+ * @b: inputs another number
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	printf(a*b);
+}
