73ffaaa64fb987475ef466567e70014a5265f576
MutaiAllan <allankiprop175@gmail.com>
2023-01-18 05:57:54 -0800

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,19 @@
-struct dog
+/**
+ * struct dog - structure definition of a dog
+ * @name: name string
+ * @age: the age float
+ * @owner: owner string
+ */
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
