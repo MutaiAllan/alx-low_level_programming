8a0c34ba80546fe779633b3d053adfb233d3ee7c
MutaiAllan <allankiprop175@gmail.com>
2023-02-20 07:59:10 -0800

    Solved task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - Prints a rectangle.
+ * @size: Size of the rectangle.
+ *
+ * Return: Nothing.
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+}
